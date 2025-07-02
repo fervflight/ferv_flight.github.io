4f6e8fb8282371a06ea4c2190640a5c8adcc41a1
fervflight <valerioferretti02@gmail.com>
2025-07-02 22:29:21 +0200

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,15 +1,1 @@
-fetch('videos.json')
-.then(response => response.json())
-.then(data => {
-const container = document.getElementById('video-list');
-data.forEach(video => {
-const div = document.createElement('div');
-div.className = 'video-item';
-div.innerHTML = `
-<iframe src="https://www.youtube.com/embed/${video.youtubeId}" frameborder="0" allowfullscreen></iframe>
-<p>${video.title}</p>
-`;
-container.appendChild(div);
-});
-})
-.catch(error => console.error('Errore caricamento video:', error));
+
